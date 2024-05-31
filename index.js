6ddd6d4b920a07c0ceda28e6251ed72ed08e3a1d
Raymond <sarge0087@gmail.com>
2024-05-30 22:29:00 -0400

    looping code along complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const name = ["Guadalupe", "Ollie", "Aki"];
+const surprise = "surprise"
+
+function writeCards(name, surprise){
+    const message = [];
+    for (let i = 0; i < name.length; i++) {
+        message[i]= (`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`);
+    }
+
+    return message;
+}
+
+writeCards();
+
+function countDown(){
+    let number = 10;
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
+
+countDown();
